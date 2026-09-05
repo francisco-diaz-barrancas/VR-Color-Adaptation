6cafa78e8cac098b62a4f15a9b6cb95266759994
Francisco Díaz Barrancas <58009827+francisco-diaz-barrancas@users.noreply.github.com>
2026-09-05 13:21:22 +0200

    Delete R directory

--- R/04_filtros.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-datos_filtrado <- datos_final %>% filter(!is.na(Edad), Edad <= 60)
-rendimiento <- datos_filtrado %>% group_by(Sujeto) %>%
-  summarise(aciertos=sum(Valor==1,na.rm=TRUE),accuracy=mean(Valor,na.rm=TRUE),.groups="drop") %>%
-  arrange(desc(aciertos),desc(accuracy))
-top10 <- slice_head(rendimiento,n=10)
-datos_top10 <- datos_filtrado %>% filter(Sujeto %in% top10$Sujeto)
-print(top10)
